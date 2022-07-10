fcfba47b8ef8f9f2337a626b0d0b39c435fc64a4
DhrubCoder <86871406+DhrubCoder@users.noreply.github.com>
2022-07-10 08:41:03 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,29 @@
+/* const accessCode1 = "JENU";
+const accessCode2 = "ZENA";
+const accessCode3 = "AGRABAH"; */
+
+const accessCode1 = "JENU";
+const accessCode2 = "AGRABAH";
+const accessCode3 = "ZENA";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("Who was found by Alladin in 'CHIRAAG' ", 100,50)
+    fill("lightblue")
+    text("Hint: E U J N", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("Where was Alladin born", 700,150)
+    fill("lightblue")
+    text("Hint: R A B A H A G", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("Who was Alladin's Mother ", 100,250)
+    fill("lightblue")
+    text("Hint: E A Z N", 100,270)
+
+}
